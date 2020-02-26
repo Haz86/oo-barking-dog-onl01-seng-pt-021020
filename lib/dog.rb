6b5a3b9e9c5c03dac35ed8cec599b6e8181acc40
Hussain Ahmed <haz6798@icloud.com>
2020-02-26 04:49:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+
+class Dog
+  
+  def bark
+    
+    puts "woof!"
+    
+  end
+
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+
+fields_for = Dog.new
+fido.name= "Fido"
+ 
+lassie.name
